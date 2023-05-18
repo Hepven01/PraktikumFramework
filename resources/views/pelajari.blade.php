0a2ac001e614541bf8e8d66474a197f6e42ae02c
Enzo <hepvenolin@gmail.com>
2023-05-18 21:10:37 +0700

    commit pertama

---
+++ resources/views/pelajari.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cendana Villas 2</title>
+    <link rel="stylesheet" href="{{asset('css/style2.css')}}">
+</head>
+<body>
+    <nav>
+        <div class="wrapper">
+            <div class="logo"><a href="{{ url('/') }}">Cendana Villas</a></div>
+            <div class="menu"></div>
+        </div>
+    </nav>
+    <div class="video">
+        <iframe class="yt border border-dark" width="560" height="315" src="https://www.youtube.com/embed/k_P1YDYAlss" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe >
+        <h1>Cendana Villas</h1>
+        <p>VISI : Menjadi manajemen Villa yang inovatif dengan sumber daya manusia yang berkualitas dengan standar internasional dan keramahtamahan khas Indonesia, serta Kami ingin menjadi pemimpin di jenjang internasional dan menjadi mitra bagi para tamu, kolega, serta mitra bisnis kami.</p>
+        <P>Misi : Untuk melayani para tamu kami setiap saat dengan menciptakan ide yang otentik dan penuh keterlibatan guna mendorong serta memberikan pengalaman tak tertandingi dari Cendana Villas.</P>
+    </div>
+</body>
+</html>
